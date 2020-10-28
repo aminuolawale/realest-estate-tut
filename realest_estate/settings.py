a779bad5551816f604a3920b92a6a9aab4a87c2a
Mohammed Aminu <aminuolawalekan@gmail.com>
2020-10-28 21:02:06 +0100

    added csrf

--- realest_estate/settings.py
+++ realest_estate/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'realest_estate.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-# CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
